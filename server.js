9407ea476292ebec93bb7829b6c44bb3a6b01445
Nora <norapuskas@yahoo.com>
2018-10-12 00:01:30 +0200

    feat(devconnector): create route files with express router

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -12,5 +15,11 @@ mongoose.connect(db)
   .catch(error => console.log(error));
 
 app.get('/', (req, res) => res.send('Hello!'));
+
+//use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
