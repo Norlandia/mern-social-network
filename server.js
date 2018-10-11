f3026327d207dec0bd7f33d887f924bb249e1f10
Nora <norapuskas@yahoo.com>
2018-10-11 23:36:54 +0200

    feat(devconnector): connect to mongoDB with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
+const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongoDB
+mongoose.connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(error => console.log(error));
 
 app.get('/', (req, res) => res.send('Hello!'));
 
