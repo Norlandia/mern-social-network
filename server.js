89269eb23fcadf427e88b2279c6eff9eb3ee649f
Nora <norapuskas@yahoo.com>
2018-10-13 22:18:50 +0200

    fix/feat(devconnector): fix mongoose connection, change date to default, add ability to register users

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 //DB config
 const db = require('./config/keys').mongoURI;
 
 //connect to mongoDB
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(error => console.log(error));
 
