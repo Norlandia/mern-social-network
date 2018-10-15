81a6f387c35dd228eacda36d4423a981c87c2485
Nora <norapuskas@yahoo.com>
2018-10-15 18:22:02 +0200

    feat(devconnector): create profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 //Load Profile model
-const Profile = require('../../modules/profile');
+const Profile = require('../../modules/Profile');
+//Load User model
+const User = require('../../modules/User');
 
 //@route GET api/profile/test
 //@desc Tests profile route
@@ -13,6 +15,28 @@ router.get('/test', (req, res) =>
   res.json({
     msg: 'Profile Works',
   })
+);
+
+//@route GET api/profile
+//@desc Get current users profile
+//@access Private
+
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.noprofile = 'There is no Profile for this user!';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch((error) => res.status(404).json(error));
+  }
 );
 
 module.exports = router;
