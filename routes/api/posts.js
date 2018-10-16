e6bb2684b10eccabba103c24f0d9e75b0c3e1ac6
Nora <norapuskas@yahoo.com>
2018-10-17 00:06:50 +0200

    feat(devconnector): create routes for finding posts and finding post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const Post = require('../../models/Post');
 
 //Validation
-const validatePostInput = require('../../validation/post'); 
+const validatePostInput = require('../../validation/post');
 
 //@route GET api/posts/test
 //@desc Tests post route
@@ -18,26 +18,53 @@ router.get('/test', (req, res) =>
   })
 );
 
+//@route GET api/posts
+//@desc Get post
+//@access Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then((posts) => res.json(posts))
+    .catch((error) =>
+      res.status(404).json({ nopostsfound: 'No posts found!' })
+    );
+});
+
+//@route GET api/posts/:id
+//@desc Get post by id
+//@access Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((error) =>
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
+    );
+});
+
 //@route POST api/posts
 //@desc Create post
 //@access Private
-router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-  const { errors, isValid } = validatePostInput(req.body);
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
 
-  //Check validation
-  if (!isValid) {
-    //If any errors, send 400 with errors object
-    return res.status(400).json(errors);
+    //Check validation
+    if (!isValid) {
+      //If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id,
+    });
+
+    newPost.save().then((post) => res.json(post));
   }
-
-  const newPost = new Post({
-    text: req.body.text,
-    name: req.body.name,
-    avatar: req.body.avatar,
-    user: req.user.id
-  });
-
-  newPost.save().then(post => res.json(post));
-});
+);
 
 module.exports = router;
